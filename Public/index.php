7d25b2fa3b71c1ff776c19b5b142191cf904132d
heval22 <hb248@njit.edu>
2019-03-03 22:51:48 -0500

    Feature: reading CSV file

--- Public/index.php
+++ Public/index.php
@@ -2,80 +2,121 @@
 /**
  * Created by PhpStorm.*/
 
-main::start();
+main::start("sales.csv");
 
 class main {
 
-    static public function start() {
-        $records = csv::getRecords();
+    static public function start($filename) {
+
+        $records = csv::getRecords($filename);
         $table = html::generateTable($records);
-        system::printPage($table);
+
+
 
     }
 
-
-}
-
-class csv {
-
-    static public function getRecords() {
-
-        $make = 'Lambo';
-        $model = 'Bling';
-        $car = AutomobileFactory::create($make, $model);
-
-        $records[] = $car;
-
-        print_r($records);
-
-        return $records;
-    }
 
 }
 
 class html {
 
-    static public function generateTable($records) {
+    public static function generateTable($records) {
 
-        $table = $records;;
+        $count = 0;
 
-        return $table;
+        foreach ($records as $record) {
 
+            if($count == 0) {
+
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+
+                print_r($values);
+
+            }
+            $count++;
+
+        }
     }
 
 }
+class csv {
 
-class system {
+    static public function getRecords($filename) {
 
-    static public function printPage($page) {
+        $file = fopen($filename,"r");
 
-        echo $page;
+        $fieldNames = array();
+
+        $count = 0;
+
+        while(! feof($file))
+        {
+
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+                }
+            $count++;
+        }
+
+        fclose($file);
+        return $records;
 
     }
+
+
 
 }
 
-class Automobile
-{
-    private $vehicleMake;
-    private $vehicleModel;
 
-    public function __construct($make, $model)
+class record {
+
+    public function __construct(Array $fieldNames = null, $values = null)
     {
-        $this->vehicleMake = $make;
-        $this->vehicleModel = $model;
+
+        $record = array_combine($fieldNames, $values);
+
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+
+        }
+
+
     }
 
-    public function getMakeAndModel()
-    {
-        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    public function  returnArray() {
+        $array = (array) $this;
+
+        return $array;
+
+    }
+
+    public function createProperty($name = 'first', $value = 'Heval') {
+
+        $this->{$name} = $value;
+
     }
 }
 
-class AutomobileFactory
-{
-    public static function create($make, $model)
-    {
-        return new Automobile($make, $model);
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+
+
+        $record = new record($fieldNames, $values);
+
+
+        return $record;
+
+
     }
 }
